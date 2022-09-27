b8c40b8dfff51c7cb7a290e17555d1cd3ff10839
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-09-27 12:28:21 +0200

    Trooper properties created

---
+++ GUI_week_4/Models/Trooper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_week_4.Models
+{
+    public class Trooper
+    {
+        private string type;
+        private int speed;
+        private int cost;
+        private int power;
+        private Hostile hostile;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+
+        public Hostile Hostile
+        {
+            get { return hostile; }
+            set { hostile = value; }
+        }
+
+
+    }
+
+    public enum Hostile
+    {
+        good,
+        bad,
+        natural
+    }
+}
